372228a12e2d583028d67d473ab0f0463e971985
Tommy Henderson <tommyhenderson@Tommys-MacBook-Pro.local>
2023-08-24 14:03:55 -0500

    deleted unnecessary CSS class and added comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,48 @@
 $(document).ready(function () {
 
-  // Hides "class saved" confirmation message automatically after loading page
+  // sets hour variable so function can use as reference
+  var hour = dayjs().hour();
+
+  // function that sets class of time block to "past", "present", or "future" based on real time
+  function blockUpdater() {
+
+    // runs function for each element with class "timeblock"
+    $('.timeblock').each(function () {
+
+      // changes HTML element ID from string to integer so it can be compared to hour variable
+      var blockHour = parseInt($(this).attr('id'));
+
+      // Sets class by comparing HTML ID to hour variable
+      if (blockHour === hour) {
+        $(this).removeClass('past')
+        $(this).removeClass('future')
+        $(this).addClass('present')
+      } else if (blockHour > hour) {
+        $(this).removeClass('past')
+        $(this).removeClass('present')
+        $(this).addClass('future')
+      } else if (blockHour < hour) {
+        $(this).removeClass('present')
+        $(this).removeClass('future')
+        $(this).addClass('past')
+      }
+    })
+  }
+
+  // runs blockUpdater function and re-runs every minute
+  blockUpdater()
+  setInterval(blockUpdater, 60000)
+
+
+  // Hides "save to planner" confirmation message
   $('#saved').addClass('hide')
 
-  // Function to show "class saved" message
+  // Function to show "saved to planner" message
   function showMessage() {
     $('#saved').removeClass('hide')
   }
 
-  // Function to hide "class saved" message
+  // Function to hide "saved to planner" message
   function hideMessage() {
     $('#saved').addClass('hide')
   }
@@ -16,53 +50,21 @@ $(document).ready(function () {
   // Event Listener where clicking the save button will run saveClass function
   $('.saveBtn').on('click', saveClass)
 
-  // function that uses form input as task variable and sets time variable and then commits to local memory. 
+  // function that uses form input as "task" variable and sets "time" variable and then commits to local memory. 
   function saveClass() {
     var task = $(this).siblings('.task').val();
     var time = $(this).parent().attr('id');
 
-//Also displays "Saved to planner" message which disappears after 2.5 seconds
+    // Also displays "Saved to planner" confirmation message which disappears after 2.5 seconds
     localStorage.setItem(time, task)
     showMessage()
     setTimeout(hideMessage, 2500)
   }
 
-  // sets hour variable so function can use as a reference to determine whether task is in past present or future
-var hour = dayjs().hour();
-
-// function that sets class of time block to "past", "present", or "future" based on real time
-function blockUpdater() {
-
-  // runs function for each element with class "timeblock"
-  $('.timeblock').each(function() {
-
-    // changes HTML element ID from string to integer so it can be compared to real hour
-    var blockHour = parseInt($(this).attr('id'));
-
-    // Sets class by comparing HTML ID to real hour
-    if (blockHour === hour) {
-      $(this).removeClass('past')
-      $(this).removeClass('future')
-      $(this).addClass('present')
-    } else if (blockHour > hour) {
-      $(this).removeClass('past')
-      $(this).removeClass('present')
-      $(this).addClass('future')
-    } else if (blockHour < hour) {
-      $(this).removeClass('present')
-      $(this).removeClass('future')
-      $(this).addClass('past')
-    }
-  })
-}
-
-// runs blockUpdater function and re-runs every minute
-blockUpdater()
-setInterval(blockUpdater, 60000)
-
-// Pulls time and sets it to 'currentday' element at the top of the page
+  // Pulls time and sets it to 'currentday' element at the top of the page
   $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY, h:mm'));
 
+  // Pulls data from localStorage for each time block 
   $('#9 .task').val(localStorage.getItem('9'));
   $('#10 .task').val(localStorage.getItem('10'));
   $('#11 .task').val(localStorage.getItem('11'));
